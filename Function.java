2aabb18afc9d07d0787e3607701e347f552b55e4
abbymccollam3 <144362499+abbymccollam3@users.noreply.github.com>
2024-06-04 08:51:30 -0700

    Add files via upload

---
+++ Function.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Function {
+    public static void announceDeveloperTeaTime() {
+        System.out.println("Waiting for developer tea time...");
+        System.out.println("Type random word and press enter to see tea time.");
+        Scanner input = new Scanner(System.in);
+        input.next();
+        System.out.println("It's developer tea time!");
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome to your new job!");
+        announceDeveloperTeaTime();
+
+        System.out.println("Write code.");
+        System.out.println("Review code.");
+
+        announceDeveloperTeaTime();
+
+        System.out.println("Get promoted!");
+    }
+}
