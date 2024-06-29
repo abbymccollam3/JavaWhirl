ef6ac456fbed2c5dffd66b752ab2cc79dbe16894
abbymccollam3 <144362499+abbymccollam3@users.noreply.github.com>
2024-06-29 09:40:37 -0700

    Create IfElse.java

---
+++ Program Flow/IfElse.java
@@ -0,0 +1,18 @@
+public class IfElse {
+    public static void main(String[] args) {
+
+        int monthNum = 4;
+
+        if (monthNum >= 1 && monthNum <= 3) {
+                System.out.println("You're in Q1");
+
+        }   else if (monthNum >= 4 && monthNum <= 6) {
+                System.out.println("You're in Q2");
+        
+        }   else {
+                System.out.println("Unknown");
+
+        }
+        
+    }
+}
